6711acc07ab37f34836945e3ece438ebd8bac989
ROHIT A S NAIR <rohitasnair111@gmail.com>
2019-10-04 22:37:43 +0530

    added grep(for firebase url) and eval

--- firebase.sh
+++ firebase.sh
@@ -3,3 +3,5 @@ for a in *.apk; do echo $a; done
 #decompile
 for a in *.apk; do apktool d $a; done
 #grep firebase url
+grep_output="grep -rnw  -e 'firebase_database_url' | grep https | cut -d '>' -f2 | cut -d '<' -f1"
+eval_output=$(eval "$grep_output")

5c91a4dab2eadd5af461041c8c0c173e439f05f4
ROHIT A S NAIR <rohitasnair111@gmail.com>
2019-10-04 22:51:05 +0530

    added only url check

---
+++ check.sh
@@ -0,0 +1,8 @@
+#grep firebase url
+grep_output="grep -rnw  -e 'firebase_database_url' | grep https | cut -d '>' -f2 | cut -d '<' -f1"
+eval_output=$(eval "$grep_output")
+#optional
+echo "$eval_output"
+#scanning for open databaseand printing
+for url in $eval_output ; do curl $url/.json?shallow=true; done
+

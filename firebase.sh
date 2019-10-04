92b24b108c0e1b2947f2d5a0732738daa8293a1e
ROHIT A S NAIR <rohitasnair111@gmail.com>
2019-10-04 22:41:31 +0530

    added curl http call

--- firebase.sh
+++ firebase.sh
@@ -5,3 +5,8 @@ for a in *.apk; do apktool d $a; done
 #grep firebase url
 grep_output="grep -rnw  -e 'firebase_database_url' | grep https | cut -d '>' -f2 | cut -d '<' -f1"
 eval_output=$(eval "$grep_output")
+#optional
+echo "$eval_output"
+#scanning for open databaseand printing
+for url in $eval_output ; do curl $url/.json?shallow=true; done
+
